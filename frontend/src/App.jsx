44f23301824bee6d2709a7f3a933ae3c1bbc5036
Esther Garcia <cyberluxmundi@proton.me>
2025-04-16 09:50:18 -0300

    config files changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './index.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
 import SignUp from './pages/signup/SignUp'
+import React from 'react'
 
 function App() {
 
